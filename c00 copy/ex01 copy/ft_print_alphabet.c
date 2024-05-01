bc5724b6d902492e782b6447b8012b124383db46
Nikhil Kunnathu <nkunnath@lab2r1s4.42abudhabi.ae>
2024-05-01 20:19:52 +0400

    All the work done until day 3

---
+++ c00 copy/ex01 copy/ft_print_alphabet.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nkunnath <nkunnath@student.42abudhabi.ae>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/01 13:30:22 by nkunnath          #+#    #+#             */
+/*   Updated: 2024/05/01 16:19:35 by nkunnath         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_alphabet(void)
+{
+	char	letter;
+
+	letter = 'a';
+	while (letter <= 'z')
+	{
+		write(1, &letter, 1);
+		letter += 1;
+	}
+}
